655d159fe20422b6dbd7b3717a1da030955df564
zhouyuzeng <zhouyuzeng@makenv.com>
2018-11-01 10:34:20 +0800

    bs

--- bs/monitor_es.sh
+++ bs/monitor_es.sh
@@ -4,13 +4,13 @@ ES_PID=`ps -ef |grep elasticsearch.pid |grep -v 'grep' |awk '{print $2}'`
 ESMonitorLog=/opt/logs/es-monitor.log
 
 StartES='/etc/init.d/elasticsearch restart'
-
+CurrentDate=`date +'%F %T'`
 Monitor()
 {
   if [[ $ES_PID ]];then
-    echo "[info]当前ES进程ID为:$ES_PID"
+    echo "[info][$CurrentDate]当前ES进程ID为:$ES_PID"
   else
-    echo "[error]ES进程不存在!ES开始自动重启..."
+    echo "[error][$CurrentDate]ES进程不存在!ES开始自动重启..."
     sh $StartES -d
   fi
 }
